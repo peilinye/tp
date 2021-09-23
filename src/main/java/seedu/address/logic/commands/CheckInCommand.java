66b0a07f28a8af478108f0cb376bc95aeb495023
BananaTechs <tehogben@gmail.com>
2021-09-23 12:21:52 +0800

    Change new commands to throw 'not implemented' errors

--- src/main/java/seedu/address/logic/commands/CheckInCommand.java
+++ src/main/java/seedu/address/logic/commands/CheckInCommand.java
@@ -6,8 +6,19 @@ import seedu.address.model.Model;
 public class CheckInCommand extends Command{
     public static final String COMMAND_WORD = "checkin";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Checks in people to a room "
+            + "using their index numbers used in the last person listing.\n"
+            + "Parameters: INDEX_ROOM (must be a positive integer) "
+            + "g/ [GUEST_INDEX]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "g/ 43 g/ 22";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "CheckIn command not implemented yet";
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("checka checka");
+       throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
--- src/main/java/seedu/address/logic/commands/CheckOutCommand.java
+++ src/main/java/seedu/address/logic/commands/CheckOutCommand.java
@@ -6,8 +6,17 @@ import seedu.address.model.Model;
 public class CheckOutCommand extends Command {
     public static final String COMMAND_WORD = "checkout";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Checks out all the people in a room "
+            + "using their index numbers used in the last person listing.\n"
+            + "Parameters: INDEX_ROOM (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "CheckOut command not implemented yet";
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("nice kicks");
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
