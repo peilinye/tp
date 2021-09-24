8871eb9f9a1e23741b8fc88efe6a51fce662fb4a
BananaTechs <tehogben@gmail.com>
2021-09-24 19:42:20 +0800

    Add JUnit tests for CheckInCommand failures

--- src/test/java/seedu/address/logic/commands/CheckInCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CheckInCommandTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -8,17 +10,13 @@ import seedu.address.model.person.Person;
 import seedu.address.model.room.Room;
 import seedu.address.model.room.Vacancy;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ROOM;
@@ -31,8 +29,8 @@ public class CheckInCommandTest {
 
     @Test
     public void execute_validRoomIndexValidGuestList_success() {
-        List guestList = Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON);
-        CheckInCommand checkInRoomOneCommand = new CheckInCommand(INDEX_FIRST_ROOM, guestList);
+        List<Index> guestList = Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON);
+        CheckInCommand checkInCommand = new CheckInCommand(INDEX_FIRST_ROOM, guestList);
 
         Room roomToEdit = model.getFilteredRoomList().get(INDEX_FIRST_ROOM.getZeroBased());
 
@@ -47,12 +45,43 @@ public class CheckInCommandTest {
 
         String expectedMessage = String.format(CheckInCommand.MESSAGE_CHECKIN_SUCCESS, editedRoom);
 
-        assertCommandSuccess(checkInRoomOneCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(checkInCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidRoomIndex_throwsCommandException() {
+        Index invalidRoomIndex = Index.fromZeroBased(model.getFilteredRoomList().size());
+
+        List<Index> guestList = Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON);
+
+        CheckInCommand checkInCommand = new CheckInCommand(invalidRoomIndex, guestList);
+
+        assertCommandFailure(checkInCommand, model, Messages.MESSAGE_INVALID_ROOM_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidGuestIndex_throwsCommandException() {
+        Index invalidGuestIndex = Index.fromZeroBased(model.getFilteredPersonList().size());
+
+        List<Index> guestList = Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON, invalidGuestIndex);
+
+        CheckInCommand checkInCommand = new CheckInCommand(INDEX_FIRST_ROOM, guestList);
+
+        assertCommandFailure(checkInCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_noGuests_throwsCommandException() {
+        List<Index> guestList = Collections.emptyList();
+
+        CheckInCommand checkInCommand = new CheckInCommand(INDEX_FIRST_ROOM, guestList);
+
+        assertCommandFailure(checkInCommand, model, CheckInCommand.MESSAGE_NO_GUESTS);
     }
 
     @Test
     public void equals() {
-        List guestList = Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON);
+        List<Index> guestList = Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON);
         CheckInCommand checkInRoomOneCommand = new CheckInCommand(INDEX_FIRST_ROOM, guestList);
         CheckInCommand checkInRoomTwoCommand = new CheckInCommand(INDEX_SECOND_ROOM, guestList);
 
