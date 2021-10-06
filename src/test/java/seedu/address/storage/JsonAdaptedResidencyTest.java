75e60f4d21dcd82ee08ff7c18ac4674defaa6e67
BananaTechs <tehogben@gmail.com>
2021-10-06 21:13:08 +0800

    Update JUnit test for JsonAdaptedResidencyBook

--- src/test/java/seedu/address/storage/JsonAdaptedResidencyTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedResidencyTest.java
@@ -2,6 +2,7 @@ package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalRooms.ROOM_ONE;
 
 import java.util.HashMap;
@@ -11,7 +12,6 @@ import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.person.Id;
 import seedu.address.model.person.Person;
 import seedu.address.model.residency.Residency;
@@ -20,22 +20,23 @@ import seedu.address.model.room.RoomNumber;
 
 public class JsonAdaptedResidencyTest {
 
-    private AddressBook addressBook = new AddressBook();
-
     @Test
     public void toModelType_validResidency_returnsResidency() {
         Set<Person> guests = new HashSet<>();
         guests.add(ALICE);
+        guests.add(BENSON);
 
-        addressBook.addPerson(ALICE);
-        addressBook.addRoom(ROOM_ONE);
         Residency residency = new Residency(ROOM_ONE, guests);
         JsonAdaptedResidency jsonResidency = new JsonAdaptedResidency(residency);
 
         Map<Id, Person> idPersonMap = new HashMap<>();
         Map<RoomNumber, Room> roomNumberRoomMap = new HashMap<>();
         idPersonMap.put(ALICE.getId(), ALICE);
+        idPersonMap.put(BENSON.getId(), BENSON);
         roomNumberRoomMap.put(ROOM_ONE.getRoomNumber(), ROOM_ONE);
+
+        Residency test = jsonResidency.toModelType(idPersonMap, roomNumberRoomMap);
+        System.out.println(test.getGuests());
 
         assertEquals(residency, jsonResidency.toModelType(idPersonMap, roomNumberRoomMap));
     }
