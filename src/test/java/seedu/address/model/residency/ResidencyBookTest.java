42bfe6035d7ea4981c73d1e0fea0743ad9f509c6
BananaTechs <tehogben@gmail.com>
2021-10-06 18:10:57 +0800

    Add JUnit tests for register() in ResidencyBook

--- src/test/java/seedu/address/model/residency/ResidencyBookTest.java
+++ src/test/java/seedu/address/model/residency/ResidencyBookTest.java
@@ -1,10 +1,21 @@
 package seedu.address.model.residency;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalRooms.ROOM_ONE;
+import static seedu.address.testutil.TypicalRooms.ROOM_TWO;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.person.Person;
+import seedu.address.model.residency.exceptions.DuplicatePersonRegException;
+import seedu.address.model.residency.exceptions.DuplicateRoomRegException;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class ResidencyBookTest {
 
@@ -13,5 +24,63 @@ public class ResidencyBookTest {
     @Test
     public void constructor() {
         assertEquals(Collections.emptyList(), residencyBook.asUnmodifiableObservableList());
+    }
+
+    @Test
+    public void register_validRoomValidGuests_success() {
+        Set<Person> guests = new HashSet<>();
+
+        guests.add(ALICE);
+        guests.add(BENSON);
+
+        residencyBook.register(ROOM_ONE, guests);
+
+        Optional<Residency> residencyRoomOne = residencyBook.getResidency(ROOM_ONE);
+        Optional<Residency> residencyAlice = residencyBook.getResidency(ALICE);
+        Optional<Residency> residencyBenson = residencyBook.getResidency(BENSON);
+
+        assertTrue(residencyRoomOne.isPresent());
+        assertTrue(residencyAlice.isPresent());
+        assertTrue(residencyBenson.isPresent());
+
+        assertEquals(residencyRoomOne.get(), residencyAlice.get());
+        assertEquals(residencyRoomOne.get(), residencyBenson.get());
+    }
+
+    @Test
+    public void register_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class,
+                () -> residencyBook.register(null, new HashSet<Person>()));
+        assertThrows(NullPointerException.class,
+                () -> residencyBook.register(ROOM_ONE, null));
+    }
+
+    @Test
+    public void register_personAlreadyRegistered_throwsDuplicatePersonRegException() {
+        Set<Person> guestsRoomOne = new HashSet<>();
+        Set<Person> guestsRoomTwo = new HashSet<>();
+
+        guestsRoomOne.add(ALICE);
+        guestsRoomOne.add(BENSON);
+        guestsRoomTwo.add(BENSON);
+        guestsRoomTwo.add(CARL);
+
+        residencyBook.register(ROOM_ONE, guestsRoomOne);
+        assertThrows(DuplicatePersonRegException.class,
+                () -> residencyBook.register(ROOM_TWO, guestsRoomTwo));
+    }
+
+    @Test
+    public void register_roomAlreadyRegistered_throwsDuplicateRoomRegException() {
+        Set<Person> guestsRoomOne = new HashSet<>();
+        Set<Person> guestsRoomTwo = new HashSet<>();
+
+        guestsRoomOne.add(ALICE);
+        guestsRoomOne.add(BENSON);
+        guestsRoomTwo.add(CARL);
+
+        residencyBook.register(ROOM_ONE, guestsRoomOne);
+        assertThrows(DuplicateRoomRegException.class,
+                () -> residencyBook.register(ROOM_ONE, guestsRoomTwo));
     }
 }
