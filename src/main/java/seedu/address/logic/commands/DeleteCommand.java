57710dc7045b57dac0c7545017a86f42efb74f17
BananaTechs <tehogben@gmail.com>
2021-10-11 21:56:41 +0800

    Prevent deletion of Persons currently checked into a room

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -3,12 +3,14 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
+import java.util.Optional;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+import seedu.address.model.residency.Residency;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
@@ -23,6 +25,7 @@ public class DeleteCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_PERSON_CHECKED_IN = "%1$s is still checked into Room %2$s";
 
     private final Index targetIndex;
 
@@ -40,6 +43,13 @@ public class DeleteCommand extends Command {
         }
 
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+
+        Optional<Residency> residency = model.getResidency(personToDelete);
+        if (residency.isPresent()) {
+            throw new CommandException(
+                    String.format(MESSAGE_PERSON_CHECKED_IN, personToDelete.getName(), residency.get().getRoom()));
+        }
+
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
     }
